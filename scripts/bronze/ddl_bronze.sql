3331d6035ff63b74324b6452f223d8ab1d9f75d0
kondor <125427575+kondor-1@users.noreply.github.com>
2025-12-09 18:25:37 -0600

    Create tables for bronze layer in data warehouse
    This SQL script creates multiple tables for the bronze layer of the data warehouse, including product information, sales details, customer data, location data, and category information.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,47 @@
+ /*
+ This creates the tables for the bronze layer of the data warehouse
+ */
+
+
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE
+
+);
+
+CREATE TABLE bronze.crm_sales_details(
+sls_ord_num	 NVARCHAR(50),
+sls_prd_key  NVARCHAR(50),
+sls_cust_id  INT,
+sls_order_dt DATE,
+sls_ship_dt  DATE,
+sls_due_dt   DATE,
+sls_sales    INT,
+sls_quantity INT,
+sls_price    INT
+
+);
+
+ CREATE TABLE bronze.erp_cust_az12 (
+ cid	NVARCHAR(50),
+ bdate  DATE,
+ gen	NVARCHAR(50)
+
+ );
+
+ CREATE TABLE bronze.erp_loc_a101 (
+ cid	NVARCHAR(50),
+ cntry  VARCHAR(50),
+ );
+
+ CREATE TABLE bronze.erp_cat_g1v2 (
+ id			 NVARCHAR(50),
+ cat		 NVARCHAR(50),
+ subcat		 NVARCHAR(50),
+ maintenance NVARCHAR(50)
+ );
